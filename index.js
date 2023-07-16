ad496055d993afb6d77aa3f6e13a4fe6117e4151
josh <Gabrielmarvelous1@gmail.com>
2023-07-15 17:27:10 -0700

    updated server

--- index.js
+++ index.js
@@ -107,11 +107,11 @@ app.post('/delete', async(req, res) => {
     res.redirect('/')
 });
 
-// let port = process.env.PORT;
-// if(port === null || port === ""){
-//   port = 3000;
-// }
+let port = process.env.PORT;
+if(port === null || port === ""){
+  port = 3000;
+}
 // Start the server
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Server is running on port 3000`);
 });
