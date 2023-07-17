7069a8f16322c19a9bcddc8e3d3be12eebcae40b
josh <Gabrielmarvelous1@gmail.com>
2023-07-16 22:29:09 -0700

    updated port

--- index.js
+++ index.js
@@ -139,6 +139,6 @@ if(port === null || port === ""){
   port = 3000;
 }
 // Start the server
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Server is running on port 3000`);
 });
